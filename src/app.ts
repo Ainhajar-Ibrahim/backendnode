9433b7e744f3d80b0de0057464afa0ef8cc5fd1f
ibrahimainhajar1 <ibrahimainhajar1@gmail.com>
2022-04-05 02:39:10 +0000

    final

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,14 @@ const db= config.get("dbUri") as string;
 const app= express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*", );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use('/api/items', items)
-
-
 
 mongoose
         .connect(db)
